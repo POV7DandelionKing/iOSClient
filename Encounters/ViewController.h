50060dfb73b328b5da1cc6521944fa49c2982fe4
Amos Latteier <amos@latteier.com>
2014-11-09 13:33:50 -0500

    Add start of animated TV

--- Encounters/ViewController.h
+++ Encounters/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "ServerHandler.h"
 @interface ViewController : UIViewController <ServerDelegate>
-
+@property(nonatomic, weak) IBOutlet UIImageView *tvView;
 - (IBAction)showVideo:(id)sender;
 @end
 
