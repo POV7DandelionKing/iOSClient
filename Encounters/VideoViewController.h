d02ff9926696f35089b0ba4caa61e8b5f4543856
Amos Latteier <amos@latteier.com>
2014-11-09 12:25:17 -0500

    grabbing wanted changes from master

---
+++ Encounters/VideoViewController.h
@@ -0,0 +1,14 @@
+//
+//  VideoViewController.h
+//  Encounters
+//
+//  Created by Amos Latteier on 2014-11-09.
+//  Copyright (c) 2014 cmyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVKit/AVKit.h>
+
+@interface VideoViewController : AVPlayerViewController
+
+@end
